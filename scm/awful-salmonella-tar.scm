a31abbd3d1767d54116fc6d43921c7a69acc894c
Mario Domenech Goulart <mario.goulart@gmail.com>
2014-03-18 22:40:25 -0300

    Handle "incomplete" request paths (e.g., /master/gcc)
    List directory contents when the requested path is a directory (unless
    it is a directory in the tar file)

--- scm/awful-salmonella-tar.scm
+++ scm/awful-salmonella-tar.scm
@@ -1,7 +1,3 @@
-;;; TODO
-;; * manage cache size
-;; * handle "incomplete" request paths (e.g., /master/gcc)
-
 ;; Core units
 (use data-structures extras files irregex posix srfi-1 srfi-13 utils)
 
@@ -12,6 +8,10 @@
 (define cache-dir (make-parameter "cache"))
 
 (define salmonella-reports-dir (make-parameter "reports"))
+
+(define salmonella-report-dir (make-parameter "salmonella-report"))
+
+(define report-tar-filename (make-parameter "salmonella-report.tar.gz"))
 
 (define report-compressor
   ;; #f specifies no compression (plain tar file)
@@ -27,23 +27,40 @@
 (define (path-split path)
   (string-split path "/"))
 
+(define safe-path?
+  ;; Just in case, since we are using the external program tar
+  (let ((safe-chars
+         (string->list "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ0123456789-+/:._")))
+    (lambda (path)
+      (and (not (substring-index ".." path))
+           (let ((path-chars (string->list path)))
+             (every (lambda (path-char)
+                      (memq path-char safe-chars))
+                    path-chars))))))
+
 (define (requested-path->tar-file requested-file-path)
   (define (tar-file-path path-parts)
     (make-pathname (list (root-path)
                          (salmonella-reports-dir)
                          (path-join path-parts))
-                   "salmonella-report.tar"))
+                   (report-tar-filename)))
   (let ((path-parts (path-split requested-file-path)))
     (if (or (null? path-parts)
-            (null? (cdr path-parts)))
+            (null? (cdr path-parts))
+            (not (safe-path? requested-file-path)))
         #f
-        (let ((dir-part (last (butlast path-parts))))
-          (case (string->symbol dir-part)
-            ((salmonella-report)
-             (tar-file-path (butlast path-parts)))
-            ((dep-graphs install ranks rev-dep-graphs test)
-             (tar-file-path (drop-right path-parts 3)))
-            (else #f))))))
+        (cond ((equal? "index.html" (last path-parts))
+               (tar-file-path (drop-right path-parts 2)))
+              ((equal? (salmonella-report-dir) (last path-parts))
+               (tar-file-path (butlast path-parts)))
+              (else
+               (let ((dir-part (last (butlast path-parts))))
+                 (case (string->symbol dir-part)
+                   ((salmonella-report)
+                    (tar-file-path (butlast path-parts)))
+                   ((dep-graphs install ranks rev-dep-graphs test)
+                    (tar-file-path (drop-right path-parts 3)))
+                   (else #f))))))))
 
 (define (split-by-salmonella-report requested-file-path)
   ;; Given /<branch>/<c-compiler>/<os>/<arch>/<yyyy>/<mm>/<dd>/salmonella-report/<report-path>,
@@ -51,16 +68,16 @@
   (let ((parts (path-split requested-file-path)))
     (let-values (((pre post) (split-at parts
                                        (list-index (lambda (elt)
-                                                     (equal? elt "salmonella-report"))
+                                                     (equal? elt (salmonella-report-dir)))
                                                    parts))))
       (cons (path-join pre)
             (path-join post)))))
 
 (define (tar-get requested-file-path)
-  (let* ((tar-file (requested-path->tar-file requested-file-path))
-         (pre/post (split-by-salmonella-report requested-file-path))
-         (pre (car pre/post))
-         (post (cdr pre/post)))
+  (and-let* ((tar-file (requested-path->tar-file requested-file-path))
+             (pre/post (split-by-salmonella-report requested-file-path))
+             (pre (car pre/post))
+             (post (cdr pre/post)))
     (and (file-exists? tar-file)
          (handle-exceptions exn
            #f
@@ -108,8 +125,47 @@
       (send-gzipped-file file)
       (send-static-file file)))
 
+(define (send-file-from-cache file-path)
+  (parameterize ((root-path (pathname-directory file-path)))
+    (let ((filename (pathname-strip-directory file-path)))
+      (send-file filename))))
 
-(define (awful-salmonella-tar base-path #!key (awful-settings (lambda (_) (_))))
+
+(define (render-dir web-dir fs-dir)
+  `(div (@ (id "content"))
+        (h2 "Index of " (code ,web-dir) ":")
+        (p (a (@ (href ,(pathname-directory web-dir)))
+              "Go to parent directory"))
+        ,(let ((dir-content (directory fs-dir)))
+           `(table
+             ,@(map (lambda (f)
+                      (let* ((fs-file (make-pathname fs-dir f))
+                             (salmonella-report-dir?
+                              (equal? f (salmonella-report-dir)))
+                             (dir? (or salmonella-report-dir?
+                                       (directory? fs-file))))
+                        `(tr
+                          (td (a (@ (href ,(make-pathname web-dir f)))
+                                 ,(if dir?
+                                      (string-append f "/")
+                                      f)))
+                          (td ,(if salmonella-report-dir?
+                                   "---"
+                                   (file-size fs-file)))
+                          (td ,(if salmonella-report-dir?
+                                   "---"
+                                   (seconds->string (file-modification-time fs-file)))))))
+                    (sort
+                     (if (member (report-tar-filename) dir-content)
+                         (cons (salmonella-report-dir) dir-content)
+                         dir-content)
+                     string<))))))
+
+(define (default-app-settings handler)
+  (parameterize ((page-css "http://wiki.call-cc.org/chicken.css"))
+    (handler)))
+
+(define (awful-salmonella-tar base-path #!key (awful-settings default-app-settings))
 
   (define base-path-pattern
     (irregex (string-append (string-chomp base-path "/") "(/.*)*")))
@@ -126,22 +182,31 @@
 
     (define-page (irregex (string-append base-path ".*"))
       (lambda (req-path)
-        (lambda ()
-          (let ((not-found (lambda ()
-                             (send-status 'not-found))))
-            (handle-exceptions exn
-              (not-found)
-              (cond ((tar-get req-path)
-                     => (lambda (file)
-                          (parameterize ((root-path (pathname-directory file)))
-                            (let ((filename (pathname-strip-directory file)))
-                              (send-file filename)))))
-                    (else
-                     (if (file-exists? (make-pathname (list (root-path)
-                                                            (salmonella-reports-dir))
-                                                      req-path))
-                         (parameterize ((root-path (salmonella-reports-dir)))
-                           (send-file req-path))
-                         (not-found)))))))))
+        (let ((fs-path (make-pathname (list (root-path)
+                                            (salmonella-reports-dir))
+                                      req-path)))
+          (if (directory? fs-path)
+              (render-dir req-path fs-path)
+              (lambda ()
+                (let ((not-found (lambda ()
+                                   (send-status 'not-found))))
+                  (handle-exceptions exn
+                    (not-found)
+                    (cond ((equal? (pathname-strip-directory req-path)
+                                   (salmonella-report-dir))
+                           (redirect-to (string-append req-path "/")))
+                          ((equal? (pathname-strip-directory (string-chomp req-path "/"))
+                                   (salmonella-report-dir))
+                           (cond ((tar-get (make-pathname req-path "index.html"))
+                                  => send-file-from-cache)
+                                 (else (not-found))))
+                          ((tar-get req-path) => send-file-from-cache)
+                          (else
+                           (if (file-exists? (make-pathname (list (root-path)
+                                                                  (salmonella-reports-dir))
+                                                            req-path))
+                               (parameterize ((root-path (salmonella-reports-dir)))
+                                 (send-file req-path))
+                               (not-found)))))))))))
     ) ;; end awful-salmonella-tar
   ) ;; end module
