e8e28036afa2b8d4ab56ee5199da49b409291864
Mario Domenech Goulart <mario.goulart@gmail.com>
2014-03-18 22:43:05 -0300

    Export parameters

--- awful-salmonella-tar.scm
+++ awful-salmonella-tar.scm
@@ -1,4 +1,12 @@
-(module awful-salmonella-tar (awful-salmonella-tar)
+(module awful-salmonella-tar
+
+  (awful-salmonella-tar
+   cache-dir
+   salmonella-reports-dir
+   salmonella-report-dir
+   report-tar-filename
+   report-compressor)
+
 (import chicken scheme)
 (include "scm/awful-salmonella-tar.scm")
 )
