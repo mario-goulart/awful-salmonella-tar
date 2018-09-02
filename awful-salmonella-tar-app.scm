2112e90475a407ebac4b0f8aeeb237789dd9aced
Mario Domenech Goulart <mario@parenteses.org>
2018-09-02 19:15:19 +0200

    Use a flat directory layout for sources

--- awful-salmonella-tar-app.scm
+++ awful-salmonella-tar-app.scm
@@ -1,7 +1,2 @@
 (use awful-salmonella-tar)
-
-;; While developing, you may want to
-;;   (load "scm/awful-salmonella-tar.scm")
-;; instead of (use awful-salmonella-tar)
-
 (awful-salmonella-tar "/")
